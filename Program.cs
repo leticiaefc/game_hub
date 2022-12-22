48a778bc519ee9ffe91cd8d0efc58bc268ed7628
Leticia Carregosa <leticiaevelynclmn@gmail.com>
2022-12-22 18:06:14 -0300

    jogo rodando

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@
                 
         static void Main(string[] args)
         {
-            Console.WriteLine("#    Jogo da velha    #");          
+            Console.WriteLine("#    Jogo da velha    #\n");          
             
             List<string> nomes = new List<string>();
             List<string> score = new List<string>();
-            int escolha;            
-            //contador de pontuação
             
+            //contador de pontuação            
             //histórico de partidas
+            int escolha;            
             
             do
             {
@@ -59,8 +59,73 @@
         }
         static void Jogo()
         {
-          Console.WriteLine("oi");
-          //opção para continuar o jogo ou retornar a tela inicial
+            string vez = "X";
+            int tentativas = 0;
+            int soma = 1;
+
+          string[,] matriz = new string[3,3]; 
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j =0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i,j] = soma.ToString();
+                    soma++;
+                }
+            } 
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"[{matriz[i,j]}] ");
+                }
+                Console.WriteLine();
+            }
+            
+            string jogada = Console.ReadLine();
+
+            Console.Clear();
+
+            while(tentativas < 9)
+            {
+                
+                for( int i = 0; i < matriz.GetLength(0); i++){
+                    for(int j =0; j< matriz.GetLength(1); j++){
+
+                        if (matriz[i,j] == jogada){
+
+                            matriz[i,j] = vez;
+
+                        }
+
+                    }
+                } // declarando a matriz e valores
+
+                for (int i =0; i < matriz.GetLength(0); i++)
+                {
+                    for ( int j = 0; j < matriz.GetLength(1);j++)
+                    {
+                        Console.Write($"[{matriz[i,j]}] ");
+                    }
+                    Console.WriteLine();
+                } // reescrevendo a matriz
+
+                jogada = Console.ReadLine();
+
+                if (vez == "X")
+                {
+                    vez = "O";
+                }
+                else
+                {
+                    vez = "X";
+                }
+
+                tentativas++;
+
+                Console.Clear();
+
+            }
         }
 
     }
